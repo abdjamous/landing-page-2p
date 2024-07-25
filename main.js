a178c09060ddfd29e684241d29f8a3b560797c08
Abd Jamous <abd.jamous@tawasulforum.org>
2024-07-25 17:01:35 +0300

    update 1.0

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  spaceBetween: 400,
   loop: true,
   autoplay: {
     delay: 3000,
@@ -17,3 +19,28 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// counters
+function animateCounter(counter, target, duration) {
+  let start = 0;
+  const increment = target / (duration / 100);
+  const interval = setInterval(() => {
+    start += increment;
+    if (start >= target) {
+      counter.innerText = target;
+      clearInterval(interval);
+    } else {
+      counter.innerText = Math.floor(start);
+    }
+  }, 100);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+  const duration = 4000; // 5 seconds
+
+  counters.forEach((counter) => {
+    const target = +counter.getAttribute("data-target");
+    animateCounter(counter, target, duration);
+  });
+});
